a6a2b1d713078bee76fb63509d8c877b5b54ecab
jill kuchman <pillanoid@gmail.com>
2016-09-21 11:32:48 -0700

    Add code to set response variable asynchronously

--- src/TwilioConsole/Program.cs
+++ src/TwilioConsole/Program.cs
@@ -5,24 +5,50 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using RestSharp;
 using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
 
 namespace TwilioConsole
 {
+    public class Message
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Body { get; set; }
+        public string Status { get; set; }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            //var authToken = "5bb0e7c6e8ed9c670a74baba7dcdf2e3";
             var client = new RestClient("https://api.twilio.com/2010-04-01");
-            var request = new RestRequest("Accounts/AC7e610a536403b0228f065c7830d4212b/Messages", Method.POST);
+            var request = new RestRequest("Accounts/AC7e610a536403b0228f065c7830d4212b/Messages.json", Method.POST);
 
             request.AddParameter("To", "+14049812892");
             request.AddParameter("From", "+14703278015");
             request.AddParameter("Body", "Hello again!");
 
             client.Authenticator = new HttpBasicAuthenticator("AC7e610a536403b0228f065c7830d4212b", "84a465c46678a067da6bfec5bebd9d4f");
-            client.ExecuteAsync(request);
+            var tcs = new TaskCompletionSource<string>();
+            String response = "";
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request);
+            }).Wait();
+            Console.WriteLine(response);
             Console.ReadLine();
         }
+
+        public static Task<string> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<string>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response.Content);
+            });
+            return tcs.Task;
+        }
+
     }
 }
